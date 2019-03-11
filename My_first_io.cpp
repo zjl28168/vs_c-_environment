ba2e70f2cbdd651d1d3072547be5edb75364d54e
ZhaoBruce <284587887@qq.com>
2019-03-11 21:12:40 +0800

    The first c++ Code

---
+++ My_first_io.cpp
@@ -0,0 +1,31 @@
+//myfirst.cpp -- display a message
+
+#include <iostream>
+//#include "stdlib.h"
+
+int my_sum_fuc( int a, int b );
+int main()
+{
+    using namespace std;
+    int num1, num2;
+    cout << "Come up and c++ me some time." << endl;
+    cout << "Please input:" << endl;
+    cout << "a=";
+    cin >> num1;
+    //cout << endl;
+    cout << "b=";
+    cin >> num2;
+    cout << "a+b=" << my_sum_fuc( num1, num2 ) << endl;
+    cout << "You won't forget it!"<<endl;
+    cin.get();
+    cin.get();
+    return 0;
+    //system（“pause”）；
+}
+
+int my_sum_fuc( int a, int b )
+{
+    int sum;
+    sum = a + b;
+    return sum;
+}
